21f9c73b9d69156477d29d269e53ffb7236cd6cf
Teema Luksameset <tm@odds.team>
2020-03-05 20:52:27 +0700

    add right total

--- src/app/pages/main/form/fee-income/fee-income.component.ts
+++ src/app/pages/main/form/fee-income/fee-income.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl } from '@angular/forms';
-import { range } from 'rxjs';
 
 @Component({
   selector: 'app-fee-income',
@@ -38,158 +37,158 @@ export class FeeIncomeComponent implements OnInit {
         id: '010',
         name: '1. สาขาวิชาคณิตศาสตร์',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '011',
         name: '- คณิตศาสตร์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '012',
         name: '- คณิตศาสตร์ประยุกต์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '020',
         name: '2. สาขาวิชาเคมี',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '021',
         name: '- เคมี',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '022',
         name: '- เคมีอินทรีย์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '023',
         name: '- วิทยาศาสตร์โพลิเมอร์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '024',
         name: '- เคมีสำหรับครู',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '030',
         name: '3. สาขาวิชาจุลชีววิทยา',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '031',
         name: '- จุลชีววิทยา',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '040',
         name: '4. สาขาวิชาชีวเคมี',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '041',
         name: '- ชีวะเคมี',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '050',
         name: '5. สาขาวิชาชีววิทยา',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '051',
         name: '- ชีวะวิทยา',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '052',
         name: '- ชีวะวิทยาสำหรับครู',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '060',
         name: '6. สาขาวิชาฟิสิกส์',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '061',
         name: '- ฟิสิกส์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '062',
         name: '- วัสดุศาสตร์และนาโนเทคโนโลยี',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '070',
         name: '7. สาขาวิทยาการคอมพิวเตอร์',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '071',
         name: '- วิทยาการคอมพิวเตอร์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '072',
         name: '- วิทยาการคอมพิวเตอร์ (ภาษาอังกฤษ)',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '073',
         name: '- เทคโนโลยีสารสนเทศและการสื่อสาร',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '074',
         name: '- เทคโนโลยีสารสนเทศ',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '075',
         name: '- ภูมิสารสนเทศศาสตร์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '076',
         name: '- การรับรู้จากระยะไกล​ ฯ',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '080',
         name: '8. สาขาวิชาวิทยาศาสตร์สิ่งแวดล้อม',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '081',
         name: '- วิทยาศาสตร์สิ่งแวดล้อม',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '090',
         name: '9. สาขาวิชาสถิติ',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '091',
         name: '- สถิติ',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '092',
         name: '- สารสนเทศสถิติ',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '093',
         name: '- สถิติประยุกต์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '100',
         name: '10. หลักสูตรนิติวิทยาศาสตร์',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '101',
         name: '- นิติวิทยาศาสตร์',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '110',
         name: '11. หลักสูตรวิทยาศาสตร์ชีวภาพ',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '111',
         name: '- วิทยาศาสตร์ชีวภาพ (นานาชาติ)',
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }, {
         id: '120',
         name: '12. สำนักงานคณบดี',
         depth: 0,
-        values: [null, null, null, null, null, null, null, null, null, null, null, null, null]
+        values: [null, null, null, null, null, null, null, null, null, null, null, null, 0]
       }
     ];
   }
@@ -203,8 +202,19 @@ export class FeeIncomeComponent implements OnInit {
             const c = j;
             this.calculateTitle(r, c);
           });
+        } else {
+          this.getFormControl(i, j).valueChanges.subscribe(() => {
+            this.calculateRightTotal(i, j);
+          });
         }
       }
+    }
+  }
+
+  calculateRightTotal(row: number, col: number) {
+    this.formModel[row].values[12] = 0;
+    for (let j = 0; j < 12; j++) {
+      this.formModel[row].values[12] += this.formModel[row].values[j];
     }
   }
 
